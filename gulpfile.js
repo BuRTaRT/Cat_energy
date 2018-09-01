b5f46be25b968292b23928a0fc690a6c8bb7ceca
Artem <burtart@ukr.net>
2018-09-01 15:48:26 +0300

    rewrited paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require("gulp");
 let sass = require("gulp-sass");
 
 gulp.task("sass", function () {
-	gulp.src("./source/scss/styles.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./source/css"))
+	gulp.src("./source/scss/main.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./source/css"))
 })
 
 gulp.task("sass:watch", function () {
