d832ee1b9d61920ebbe7af3c8e2d6391c9d0c6f8
Artem <burtart@ukr.net>
2018-10-30 01:59:01 +0200

    animation fixes

--- source/js/mob-catalog.js
+++ source/js/mob-catalog.js
@@ -1,9 +1,7 @@
-
-window.onload = function () {
     var navIcon = document.querySelector(".main-header__mob-nav");
     var nav = document.querySelector(".nav");
     navIcon.onclick = function () {
         navIcon.classList.toggle("mob-nav_hide");
         nav.classList.toggle("nav_hidden");
     }
-}
+
